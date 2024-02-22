6ba6542690f17c39f99fae8151a00ca9bad25e7f
Tanjimul Hisham <mdtanjimulhisham@gmail.com>
2024-02-22 23:33:26 +0600

    loop added

---
+++ loops.js
@@ -0,0 +1,15 @@
+// ---------------------------while loop--------------------------
+let number = 1;
+let sumation = 0;
+while(number <= 20){
+    console.log(number);
+    sumation = sumation + number;
+    console.log(sumation);
+    number += 1;
+}
+// -----------------------------for loop----------------------------
+const peopleName = ["Jonny", "Ronny", "Tanjimul", "Hisham", "Fardin", "Tonny", "Tommy", "Sins", "Doggy", "Pussy-Cat"];
+let number1 = 1;
+for(const name of peopleName){
+    console.log(name);
+}
