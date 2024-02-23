a8b4fb35622da4683299621df2ab7fc35407ad49
Tanjimul Hisham <mdtanjimulhisham@gmail.com>
2024-02-23 12:25:44 +0600

    task added

--- homework.js
+++ homework.js
@@ -1,33 +1,33 @@
 // ------------------------------------------commitment.js--------------------------------------
-// for(let num = 1; num <= 60; num++){
-//     console.log(num, "==> I will invest at least 6 hrs every single day for next 60 days!");
-// }
+for(let num = 1; num <= 60; num++){
+    console.log(num, "==> I will invest at least 6 hrs every single day for next 60 days!");
+}
 // ------------------------------------------odd.js--------------------------------------
-// for(let num = 61; num <= 100; num++){
-//     if(num % 2 === 1){
-//         console.log(num);
-//     }
-// }
+for(let num = 61; num <= 100; num++){
+    if(num % 2 === 1){
+        console.log(num);
+    }
+}
 // ------------------------------------------even.js--------------------------------------
-// for(let num = 78; num <= 98; num += 1){
-//     if(num % 2 === 0){
-//         console.log(num)
-//     }
-// }
+for(let num = 78; num <= 98; num += 1){
+    if(num % 2 === 0){
+        console.log(num)
+    }
+}
 // ------------------------------------------odd sum.js--------------------------------------
-// let sum = 0;
-// for(let num = 91; num <= 129; num += 1){
-//     if(num % 2 === 1){
-//         sum = sum + num
-//     }
-// }
-// console.log(sum)
+let sum = 0;
+for(let num = 91; num <= 129; num += 1){
+    if(num % 2 === 1){
+        sum = sum + num
+    }
+}
+console.log(sum)
 // ------------------------------------------even sum.js--------------------------------------
-// let sum = 0;
-// for(let num = 51; num <= 85; num += 1){
-//     if(num % 2 === 1){
-//         sum = sum + num;
-//     }
-// }
-// console.log(sum)
+let sum = 0;
+for(let num = 51; num <= 85; num += 1){
+    if(num % 2 === 1){
+        sum = sum + num;
+    }
+}
+console.log(sum)
 // ------------------------------------------even sum.js--------------------------------------
